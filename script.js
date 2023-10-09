02cfa440fd6e315211068f197cec91fee5caf762
Huzefa <huzefazaheer1+git@gmail.com>
2023-10-10 00:14:52 +0500

    Added new toggleable theme

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const root = document.querySelector(":root");
 const resulttxt = document.querySelector(".result");
 const buttons = document.querySelector(".calcbtns");
+const changecolorbtn = document.querySelector(".changecolor");
+let rootStyle = getComputedStyle(document.querySelector(":root"));
 
 let result = 0;
 let no1 = 0;
@@ -23,6 +26,7 @@ function Operate(no1, no2, operation) {
   }
   selectionNo = 0;
   resulttxt.innerText = result;
+  no1 = result;
   operation = "none";
 }
 
@@ -36,6 +40,7 @@ function doSpecialFunction(textSelected) {
       break;
     case "Ans":
       no1 = result;
+      resulttxt.innerHTML = result;
       selectionNo = 1;
       break;
     case "+":
@@ -76,5 +81,27 @@ buttons.addEventListener("click", (e) => {
       noSelected = Number(textSelected);
       addValue(noSelected);
     }
+  }
+});
+
+let currentTheme = "black";
+changecolorbtn.addEventListener("click", (e) => {
+  if (currentTheme == "black") {
+    root.style.setProperty("--background-color", "#C9D4DB");
+    root.style.setProperty("--button-color", "#4B6672");
+    root.style.setProperty("--special-button-color", "#E9E11E");
+    root.style.setProperty("--button-shadow-color", "#2f383c");
+    root.style.setProperty(
+      "--special-button-shadow-color",
+      "rgb(174, 168, 21)"
+    );
+    currentTheme = "white";
+  } else {
+    root.style.setProperty("--background-color", "#1a1a1a");
+    root.style.setProperty("--button-color", "#8f8c8c");
+    root.style.setProperty("--special-button-color", "#4ac556");
+    root.style.setProperty("--button-shadow-color", "#6a6a6a");
+    root.style.setProperty("--special-button-shadow-color", "rgb(52, 136, 60)");
+    currentTheme = "black";
   }
 });
