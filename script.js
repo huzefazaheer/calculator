8ca325134f2d1980f9ca2da04067a6bfe3454f5d
Huzefa <huzefazaheer1+git@gmail.com>
2023-10-09 23:27:07 +0500

    Completed operate function

--- script.js
+++ script.js
@@ -4,38 +4,8 @@ const buttons = document.querySelector(".calcbtns");
 let result = 0;
 let no1 = 0;
 let no2 = 0;
+let selectionNo = 0;
 let operation = "none";
-
-buttons.addEventListener("click", (e) => {
-  clickedElement = e.target;
-  if (clickedElement.nodeName == "BUTTON") {
-    resulttxt.innerText = clickedElement.innerText;
-    let result = "";
-    switch (clickedElement.innerText) {
-      case "AC":
-        result = "";
-        break;
-      case "=":
-        result = Operate(no1, no2, operation);
-        break;
-      case "+":
-        operation = "add";
-        break;
-      case "-":
-        operation = "sub";
-        break;
-      case "×":
-        operation = "mult";
-        break;
-      case "÷":
-        operation = "div";
-        break;
-      default:
-        return;
-    }
-  }
-  resulttxt.innerText = result;
-});
 
 function Operate(no1, no2, operation) {
   switch (operation) {
@@ -51,6 +21,60 @@ function Operate(no1, no2, operation) {
     case "div":
       no2 != 0 ? (result = no1 / no2) : (result = "error");
   }
-
-  return result;
+  selectionNo = 0;
+  resulttxt.innerText = result;
+  operation = "none";
 }
+
+function doSpecialFunction(textSelected) {
+  switch (textSelected) {
+    case "AC":
+      resulttxt.innerHTML = "";
+      break;
+    case "=":
+      Operate(no1, no2, operation);
+      break;
+    case "Ans":
+      no1 = result;
+      selectionNo = 1;
+      break;
+    case "+":
+      operation = "add";
+      break;
+    case "-":
+      operation = "sub";
+      break;
+    case "×":
+      operation = "mult";
+      break;
+    case "÷":
+      operation = "div";
+      break;
+    default:
+      return;
+  }
+}
+
+function addValue(noSelected) {
+  selectionNo == 0 ? (no1 = noSelected) : (no2 = noSelected);
+  console.log(selectionNo + ":" + no1 + "   " + operation + " " + no2);
+  selectionNo++;
+
+  //If it is first number then add value to no1
+  //If it is second number then add value to no2
+  //If it is operator then add operation
+}
+
+buttons.addEventListener("click", (e) => {
+  clickedElement = e.target;
+  textSelected = e.target.innerText;
+  if (clickedElement.nodeName == "BUTTON") {
+    resulttxt.innerText = textSelected;
+    if (isNaN(Number(textSelected))) {
+      doSpecialFunction(textSelected);
+    } else {
+      noSelected = Number(textSelected);
+      addValue(noSelected);
+    }
+  }
+});
